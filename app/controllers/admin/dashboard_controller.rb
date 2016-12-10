e42dc4515721045688790dcce45d4bf27cfdc809
madebymak <mr.jmak@gmail.com>
2016-12-10 01:25:54 +0000

    Added admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['PASS_WORD']
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['PASS_WORD']
+
   def index
     @products = Product.order(id: :desc).all
   end
